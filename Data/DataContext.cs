444a220ebf4677f70cf4d639d1bbdd32b95ba946
Saif <MOHSS@orsted.com>
2024-07-24 11:16:23 +0800

    End of Section 2

---
+++ Data/DataContext.cs
@@ -0,0 +1,9 @@
+﻿using DatingAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingAPI.Data;
+
+public class DataContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<AppUser> Users { get; set; }
+}
